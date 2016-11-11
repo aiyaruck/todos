7085d371ca3a4669f452a79296bcb487ab2c2fb3
Aiyaruck Boonlert <aiyaruck@hotmail.com>
2016-11-11 17:37:37 +0700

    somnuek

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-angular.modules('todosApp', [])
+angular.module('todoApp', [])
+  .service('todoService', function (){
+    var self = this
+    self.todo = []
+
+    self.list = function() {
+        return self.todo
+    }
+    self.summit = function(Todo) {
+        self.todo.push(Todo)
+  }
+})
+    .controller('listTodoController', function($scope, todoService) {
+       $scope.todo = todoService.list()
+   })
+   .controller('addTodoController', function($scope, todoService) {
+           $scope.title = ' '
+           $scope.submit = function() {
+               if ($scope.title !== ' '){
+                 var Todo = {title: $scope.title}
+                 todoService.summit(Todo);
+                  resetForm();
+               }
+           }
+
+           function resetForm() {
+               $scope.title = ' '
+           }
+       })
