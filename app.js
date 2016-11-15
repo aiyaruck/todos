6111a94a1722eb34cee0ee2b229e30854e67b698
Aiyaruck Boonlert <aiyaruck@hotmail.com>
2016-11-15 23:55:17 +0700

    Add all files Checked Show Complete

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 angular.module('todoApp', [])
-  .service('todoService', function (){
-    var self = this
-    self.todo = []
+    .service('todoService', function() {
+        var self = this
+        self.todo = [{
+            title: 'Somnuek',
+            complete: false
+        }, {
+            title: 'Aiyaruck',
+            complete: true
+        }]
 
-    self.list = function() {
-        return self.todo
-    }
-    self.summit = function(Todo) {
-        self.todo.push(Todo)
-  }
-})
+        self.list = function() {
+            return self.todo
+        }
+        self.submit = function(Todo) {
+            self.todo.push(Todo)
+        }
+
+        
+    })
     .controller('listTodoController', function($scope, todoService) {
-       $scope.todo = todoService.list()
-   })
-   .controller('addTodoController', function($scope, todoService) {
-           $scope.title = ' '
-           $scope.submit = function() {
-               if ($scope.title !== ' '){
-                 var Todo = {title: $scope.title}
-                 todoService.summit(Todo);
-                  resetForm();
-               }
-           }
+        $scope.todo = todoService.list()
+        $scope.Check = function(mes) {
+            todoService.Check(mes)
+        }
+    })
+    .controller('addTodoController', function($scope, todoService) {
+        $scope.title = ' '
+        $scope.submit = function() {
+            if ($scope.title !== ' ') {
+                var Todo = {
+                    title: $scope.title,
+                    complete: false
+                }
+                todoService.submit(Todo);
+                reset();
+            }
+        }
 
-           function resetForm() {
-               $scope.title = ' '
-           }
-       })
+        function reset() {
+            $scope.title = ' '
+        }
+    })
